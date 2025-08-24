ea881bd816ae76468219eb1c487341b02eca12c8
Akash Sikarwar <akash.sikarwar2807@gmail.com>
2025-08-24 22:26:25 +0530

    final touch

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -6,15 +6,15 @@ import {Link, useNavigate} from "react-router";
 import {useEffect, useState} from "react";
 
 export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "Resumind" },
-    { name: "description", content: "Smart feedback for your dream Job!"},
-  ];
+    return [
+        { title: "Resumind" },
+        { name: "description", content: "Smart feedback for your dream job!" },
+    ];
 }
 
 export default function Home() {
-    const {isLoading,auth,kv} = usePuterStore();
-    const navigate= useNavigate();
+    const { auth, kv } = usePuterStore();
+    const navigate = useNavigate();
     const [resumes, setResumes] = useState<Resume[]>([]);
     const [loadingResumes, setLoadingResumes] = useState(false);
 
@@ -39,40 +39,39 @@ export default function Home() {
         loadResumes()
     }, []);
 
+    return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+        <Navbar />
 
+        <section className="main-section">
+            <div className="page-heading py-16">
+                <h1>Track Your Applications & Resume Ratings</h1>
+                {!loadingResumes && resumes?.length === 0 ? (
+                    <h2>No resumes found. Upload your first resume to get feedback.</h2>
+                ): (
+                    <h2>Review your submissions and check AI-powered feedback.</h2>
+                )}
+            </div>
+            {loadingResumes && (
+                <div className="flex flex-col items-center justify-center">
+                    <img src="/images/resume-scan-2.gif" className="w-[200px]" />
+                </div>
+            )}
 
-  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
-      <Navbar />
-      <section className="main-section">
-          <div className="page-heading py-16">
-              <h1>Track Your Applications & Resume Ratings</h1>
-              {!loadingResumes && resumes?.length === 0 ? (
-                  <h2>No resumes found. Upload your first resume to get feedback.</h2>
-              ): (
-                  <h2>Review your submissions and check AI-powered feedback.</h2>
-              )}
-          </div>
-          {loadingResumes && (
-              <div className="flex flex-col items-center justify-center">
-                  <img src="/images/resume-scan-2.gif" className="w-[200px]" />
-              </div>
-          )}
+            {!loadingResumes && resumes.length > 0 && (
+                <div className="resumes-section">
+                    {resumes.map((resume) => (
+                        <ResumeCard key={resume.id} resume={resume} />
+                    ))}
+                </div>
+            )}
 
-          {!loadingResumes && resumes.length > 0 && (
-          <div className="resume-section">
-            {resumes.map((resume) => (
-                <ResumeCard key={resume.id} resume={resume} />
-                ))}
-          </div>
-          )}
-
-          {!loadingResumes && resumes?.length === 0 && (
-              <div className="flex flex-col items-center justify-center mt-10 gap-4">
-                  <Link to="/upload" className="primary-button w-fit text-xl font-semibold">
-                      Upload Resume
-                  </Link>
-              </div>
-          )}
-      </section>
-  </main>
+            {!loadingResumes && resumes?.length === 0 && (
+                <div className="flex flex-col items-center justify-center mt-10 gap-4">
+                    <Link to="/upload" className="primary-button w-fit text-xl font-semibold">
+                        Upload Resume
+                    </Link>
+                </div>
+            )}
+        </section>
+    </main>
 }
--- app/routes/wipe.tsx
+++ app/routes/wipe.tsx
@@ -39,10 +39,14 @@ const WipeApp = () => {
     }
 
     return (
+        <main className="bg-[url('/images/bg-main.svg')] bg-cover">
         <div>
-            Authenticated as: {auth.user?.username}
-            <div>Existing files:</div>
-            <div className="flex flex-col gap-4">
+            <h1 className="text-2xl font-semibold text-gray-700 justify-center text-center">
+            Authenticated as: {auth.user?.username}</h1>
+            <div>
+                <h2 className="text-2xl font-semibold text-gray-700 justify-center text-center">Existing files:</h2>
+                </div>
+            <div className="flex flex-col gap-4 text-sm text-gray-500 ">
                 {files.map((file) => (
                     <div key={file.id} className="flex flex-row gap-4">
                         <p>{file.name}</p>
@@ -58,6 +62,7 @@ const WipeApp = () => {
                 </button>
             </div>
         </div>
+        </main>
     );
 };
 
