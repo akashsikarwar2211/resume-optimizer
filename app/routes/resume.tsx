38d5a9f57d02ed6fa8a39433e5f0518628747373
Akash Sikarwar <akash.sikarwar2807@gmail.com>
2025-08-24 16:15:19 +0530

    Resume FeedBack Components

--- app/routes/resume.tsx
+++ app/routes/resume.tsx
@@ -3,6 +3,7 @@ import {useEffect, useState} from "react";
 import {usePuterStore} from "~/lib/puter";
 import Summary from "~/components/Summary";
 import Details from "~/components/Details";
+import ATS from "~/components/ATS";
 
 export const x = () => ([
     {title : 'Resumind | Review'},
@@ -72,7 +73,7 @@ const Resume = () => {
                     {feedback ? (
                         <div className="flex flex-col gap-8 animate-in fade-in duration-1000">
                          <Summary feedback={feedback} />
-                         <ATS score={{feedback.ATS.score || 0}} suggestions ={feedback.ATS.tips || []} />
+                         <ATS score={feedback.ATS.score || 0} suggestions ={feedback.ATS.tips || []} />
                          <Details feedback={feedback} />
                         </div>
                     ): (
